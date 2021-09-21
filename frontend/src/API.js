51d19044e0ecd00d67ea91c194d5ec1f2dd8c750
Foysal Ahmed <79821057+foysal90@users.noreply.github.com>
2021-09-21 02:51:43 -0400

    modified api

--- frontend/src/API.js
+++ frontend/src/API.js
@@ -6,7 +6,7 @@ var baseURL;
 // } else {
 //     baseURL = "http://127.0.0.1:8000";
 // }
-baseURL = "https://food-backend-production.herokuapp.com/";
+baseURL = "http://127.0.0.1:8000";
 const api = axios.create({
   baseURL: baseURL,
   headers: {
